d9181e43531fe96ff1b18d593b96878898ead47d
kartikpavan <kartik.varanasi@yahoo.com>
2022-08-01 18:06:41 +0530

    typewriter effect on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { Link } from "react-scroll";
+// import Typewriter from "typewriter-effect";
+import TypeWriterEffect from "react-typewriter-effect";
 
 const Home = () => {
 	return (
@@ -9,16 +11,39 @@ const Home = () => {
 			<div className="max-w-[1000px] mx-auto h-full p-8 flex flex-col justify-center ">
 				<p className="text-accent text-lg">Hi, my name is </p>
 				<h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-					Kartik Pavan
+					{/* 				
+					<Typewriter
+						onInit={(typewriter) => {
+							typewriter.typeString("Kartik Pavan").stop().start();
+						}}
+					/> */}
+					<TypeWriterEffect
+						startDelay={100}
+						cursorColor="#101729"
+						text="Kartik Pavan"
+						typeSpeed={100}
+					/>
 				</h1>
 				<h1 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-					I'm a full Stack Developer
+					{/* <Typewriter
+						onInit={(typewriter) => {
+							typewriter
+								.pauseFor(3000)
+								.typeString("I'm a full Stack Developer")
+								.start();
+						}}
+					/> */}
+					<TypeWriterEffect
+						startDelay={1800}
+						cursorColor="#8993af"
+						text="I'm a full Stack Developer"
+						typeSpeed={100}
+					/>
 				</h1>
 				<p className="text-[#8892b0] max-w-[700px] py-4 ">
-					I'm a full-stack developer specializing in building &#40;and
-					occassionaly designing &#41; digital experiences. Currently
-					i am focused on building responsive full stack web
-					application.
+					I'm a full-stack developer specializing in building &#40;and occassionaly
+					designing &#41; digital experiences. Currently i am focused on building
+					responsive full stack web application.
 				</p>
 				<div>
 					<button className="btn btn-accent text-xl my-2 px-4 flex gap-4">
